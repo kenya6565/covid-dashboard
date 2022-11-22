c01da3bb65eb1daad0f6e32ae1d604c6798ef0cd
kenya6565 <ken.australia0605@gmail.com>
2022-11-23 03:56:17 +0900

    create PieChart

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
-import Cards from './features/covid/Cards/Cards';
-import Chart from './features/covid/Chart/Chart';
+import Cards from "./features/covid/Cards/Cards";
+import Chart from "./features/covid/Chart/Chart";
+import PieChart from "./features/covid/PieChart/PieChart";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <header className="App-header">
         <Cards />
         <Chart />
+        <PieChart />
       </header>
     </div>
   );
